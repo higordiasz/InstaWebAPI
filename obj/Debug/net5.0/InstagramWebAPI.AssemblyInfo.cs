7ed1d193c3563ed9cf8ac48d33068bc0fe764b6c
Dias <higordiaszuqueto@gmail.com>
2021-12-14 14:24:12 -0300

    config

--- obj/Debug/net5.0/InstagramWebAPI.AssemblyInfo.cs
+++ obj/Debug/net5.0/InstagramWebAPI.AssemblyInfo.cs
@@ -13,11 +13,13 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arka")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Instagram\'s WebAPI, it was created for educational purposes only.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("InstagramWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InstagramWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/higordiasz/InstaWebAPI")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
